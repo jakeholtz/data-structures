fa8b2464863124cd1a53e0b2965d15f122cd313c
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:30:40 -0700

    finished stack in functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,36 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  var totalPassedIn = 0;
+  var deleted = 0;
 
   // Implement the methods below
-
+  debugger;
   someInstance.enqueue = function(value) {
+    count++;
+    totalPassedIn++;
+    storage[totalPassedIn] = value;
   };
 
   someInstance.dequeue = function() {
+    count > 0 ? count-- : null;
+    deleted++;
+    var first = Object.keys(storage)
+    var dequeued = storage[deleted];
+    delete storage[deleted];
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
 };
+test = new Queue();
+test.enqueue('b');
+test.dequeue();
+test.enqueue('c');
+// var expected = queue.dequeue()).to.equal('b');
+console.log(JSON.stringify(test));
