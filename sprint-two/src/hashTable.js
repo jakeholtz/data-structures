2c2aaa4bb04c0c33a7a2c0c2f77c2138f61ed963
Hack Reactor Student <student@hackreactor.com>
2017-03-31 15:54:09 -0700

    [Working on] hashTable.js. Being awesome as always.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,4 +1,4 @@
-
+1
 
 var HashTable = function() {
   this._limit = 8;
@@ -7,14 +7,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tuple = [k, v];
+  var bucket = [];
+  bucket.push(tuple);
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+    	return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+    	bucket.splice(i);
+    }
+  }
 };
 
 
